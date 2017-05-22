768d6628e0a82690ad9582360e702fcbd49bfce0
peter <pkarella27@yahoo.com>
2017-05-22 11:33:56 -0700

    add bower and additional gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,46 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 
-gulp.task('jsBrowserify', function(){
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+gulp.task('bowerCSS', function(){
+  return gulp.src(lib.ext('css').files)
+  .pipe(concat('vendor.css'))
+  .pipe(gulp.dest('./build/css'))
+})
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'], function(){
   return browserify({ entries:['./js/journal-interface.js'] })
   .bundle()
   .pipe(source('app.js'))
   .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
 });
